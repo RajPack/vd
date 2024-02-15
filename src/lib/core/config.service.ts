ab2771033e574704c0d4526370465f86af0c150e
Raj <rckraja@hotmail.com>
2024-02-15 12:48:26 +0530

    commit

--- src/lib/core/config.service.ts
+++ src/lib/core/config.service.ts
@@ -1,3 +1,5 @@
+import { BehaviorSubject, Observable, ReplaySubject, Subject, map } from "rxjs";
+
 export type Config = DashboardConfig & LayoutConfig;
 
 export type Slice<X, T extends keyof X> = X[T];
@@ -16,3 +18,49 @@ export interface LayoutConfig {
     }[];
   };
 }
+
+export class ConfigService<T> {
+  state!: T;
+  state$ = new ReplaySubject<T>(1);
+
+  constructor(initialValues: T) {
+    this.init(initialValues);
+  }
+
+  private init(initialValues: T) {
+    this.state = initialValues;
+  }
+
+  public updateState(value: Partial<T>) {
+    this.state = { ...this.state, ...value };
+    this.state$.next(this.state);
+  }
+
+  public static select(selector: (_state: T) => Observable<unknown>) {
+    return () => state$.pipe(map());
+  }
+}
+
+export function createRootState<T>(initialValue: T): BehaviorSubject<T> {
+  return new BehaviorSubject<T>(initialValue);
+}
+
+export function createRootUpdater<T>(
+  rootState$: BehaviorSubject<T>,
+  updateFn: (state: T, newState: Partial<T>) => T
+) {
+  return (_newState: Partial<T>) => {
+    const state = rootState$.getValue();
+    const newState = updateFn(state, _newState);
+    rootState$.next(newState);
+  };
+}
+
+export function createFeatureState<T, V>(
+  sourceState$: Observable<T>,
+  selector: (state: T) => V
+): Observable<V> {
+  return sourceState$.pipe(map(selector));
+}
+
+export function updateState<T>() {}
